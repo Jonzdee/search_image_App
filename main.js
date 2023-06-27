eb6b72e982bbfdc43ea46b1a18e1cf28bc54093a
Johnson Olayemi <122567059+Jonzdee@users.noreply.github.com>
2023-06-27 07:12:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const accessKey = "jWHszl7wjwDRo0FnNV0Ym-l4nso83rul0lDSwpM4U5Q";
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputDate = "";
+let page = 1;
+
+async function searchImages() {
+  inputDate = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputDate}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+  results.map((result) => {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  searchImages();
+});
+
